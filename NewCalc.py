13b6b9285493a9c3f65c273ad350d0f19f30ae97
Карпач Олег <oleg.karpach@vokigames.com>
2025-04-07 22:42:24 +0400

    1)Add zero-logic in basic 2)Change logic in new 3)Add excepts 4)Change values with forbidden types on zero

--- NewCalc.py
+++ NewCalc.py
@@ -5,55 +5,6 @@ import re
 class NewCalc(BasicCalc):
     memory = []
 
-    def __init__(self):
-        self.flag_expression = False
-        self.flag_sp = False
-        self.operation = None
-
-    @staticmethod
-    def calc_multiply(first, second=None):
-        if second is None:
-            second = BasicCalc.last_result
-        s = first * second
-        print(s)
-        BasicCalc.last_result = s
-        NewCalc.memo_plus(s)
-        NewCalc.memo_minus()
-
-    @staticmethod
-    def calc_divide(first, second=None):
-        if second is None:
-            second = BasicCalc.last_result
-        s = first / second
-        print(s)
-        BasicCalc.last_result = s
-        NewCalc.memo_plus(s)
-        NewCalc.memo_minus()
-
-    @staticmethod
-    def calc_subtract(first, second=None):
-        if second is None:
-            second = BasicCalc.last_result
-        s = first - second
-        print(s)
-        BasicCalc.last_result = s
-        NewCalc.memo_plus(s)
-        NewCalc.memo_minus()
-
-    @staticmethod
-    def calc_add(first, second=None):
-        if second is None:
-            if isinstance(first, (list, tuple)):
-                s = sum(first)
-            else:
-                s = first + BasicCalc.last_result
-        else:
-            s = first + second
-        print(s)
-        BasicCalc.last_result = s
-        NewCalc.memo_plus(s)
-        NewCalc.memo_minus()
-
     @staticmethod
     def memo_plus(number=None):
         while True:
@@ -101,92 +52,60 @@ class NewCalc(BasicCalc):
         else:
             return 'Список пуст!'
 
-    operations = {
-        '+': 'calc_add',
-        '-': 'calc_subtract',
-        '*': 'calc_multiply',
-        '/': 'calc_divide'
-    }
+    def check_input(self):
+        match = re.fullmatch(self.pattern, self.num_1)
+        if match:
+            first_num, _, operation, second_num, _ = match.groups()
+            first_num = float(first_num)
+            second_num = float(second_num)
+            res = self.operations[operation](first_num, second_num)
+            print(res)
+            self.flag_expression = True
+            self.last_result = res
+            BasicCalc.last_result = res
+            return res
 
-    def run(self):
-        self.reset_flags()
-        num_2 = None
         while True:
-            num_1 = input('Введи цифру или математическое выражение без пробелов: ')
-            match = re.fullmatch(BasicCalc.pattern, num_1)
-
-            if match:
-                try:
-                    first_num, _, operation, second_num, _ = match.groups()
-                    first_num = float(first_num) if '.' in first_num else int(first_num)
-                    second_num = float(second_num) if '.' in second_num else int(second_num)
-                    getattr(self.__class__, self.operations[operation])(first_num, second_num)
-                    self.flag_expression = True
+            for n in self.num_1:
+                n.replace('.', '', 1)
+                if n.isalpha() or n == '.':
+                    print(f'Некорректное значение "{self.num_1}", заменено на 0')
+                    self.num_1 = '0'
                     break
-                except ZeroDivisionError:
-                    print("Ошибка: Деление на ноль! Повторите ввод.")
-                    continue
+
+            if len(self.num_1) > 1 and ' ' in self.num_1:
+                self.num_1 = [int(n) for n in self.num_1.split()]
+                self.flag_sp = True
+                break
             else:
-                if len(num_1) > 1 and ' ' in num_1:
-                    try:
-                        num_1 = [int(n) for n in num_1.split()]
-                        self.flag_sp = True
-                        break
-                    except ValueError:
-                        print(f'Некорректное значение "{num_1}", заменено на 0')
-                        num_1 = [0]
-                        self.flag_sp = True
-                        break
-                else:
-                    try:
-                        num_1 = float(num_1)
-                        break
-                    except (ValueError, TypeError):
-                        print(f'Некорректное значение "{num_1}", заменено на 0')
-                        num_1 = 0
-                        break
+                self.num_1 = float(self.num_1)
+                break
 
-        if self.flag_expression is False:
-            while True:
-                self.operation = input('Выберите знак математической операции: +, -, *, /  ')
-                if self.operation not in self.operations:
-                    print('Недопустимая операция!')
-                    continue
-                else:
-                    break
-
+        while True:
             if self.flag_sp is False:
-                while True:
-                    num_2 = input('Введи цифру (или Enter для значения из памяти): ')
-                    try:
-                        if num_2 == '':
-                            num_2 = None
-                            break
-                        elif num_2.count('.') <= 1:
-                            num_2 = float(num_2)
-                            break
-                    except (ValueError, TypeError):
-                        print(f'Некорректное значение "{num_2}", заменено на 0')
-                        num_2 = 0
+                for n in self.num_2:
+                    n.replace('.', '', 1)
+                    if n.isalpha() or n == '.':
+                        print(f'Некорректное значение "{self.num_2}", заменено на 0')
+                        self.num_2 = '0'
                         break
 
-        if self.flag_expression is False:
-            try:
-                if self.flag_sp:
-                    getattr(self.__class__, self.operations[self.operation])(num_1)
-                else:
-                    getattr(self.__class__, self.operations[self.operation])(num_1, num_2)
-            except ZeroDivisionError:
-                print("Ошибка: Деление на ноль! Повторите операцию.")
-                return
+                self.num_2 = float(self.num_2)
+                break
 
 
 calc = NewCalc()
+
 while True:
-    start_off_value_input = input('Введи "Продолжить" чтобы продолжить, "Выйти" чтобы выйти, "Значение", '
+    start_off_value_input = input('Введи "Начать или Продолжить" чтобы начать или продолжить, "Выйти" чтобы выйти, "Значение", ' 
                                   'чтобы вывести верхнее значение: ').strip().upper()
-    if start_off_value_input == 'ПРОДОЛЖИТЬ':
-        calc.run()
+
+    if start_off_value_input == 'ПРОДОЛЖИТЬ' or start_off_value_input == 'НАЧАТЬ':
+        calc.set_info()
+        calc.check_input()
+        result = calc.calculate_result()
+        calc.memo_plus(result)
+        calc.memo_minus()
     elif start_off_value_input == 'ВЫЙТИ':
         break
     elif start_off_value_input == 'ЗНАЧЕНИЕ':
--- basic_calc.py
+++ basic_calc.py
@@ -2,140 +2,122 @@ import re
 
 
 class BasicCalc:
-    last_result = 0
-    memory = []
     pattern = r'^(\d+(\.\d+)?)([+\-*/])(\d+(\.\d+)?)$'
+    last_result = None
+
+    def __init__(self):
+        self.flag_expression = False
+        self.flag_sp = False
+        self.num_1_invalid = False
+        self.num_2_invalid = False
+        self.operation = None
+        self.num_1 = None
+        self.num_2 = None
+        self.pattern = BasicCalc.pattern
+
+        self.operations = {
+            '+': self.calc_add,
+            '-': self.calc_subtract,
+            '*': self.calc_multiply,
+            '/': self.calc_divide
+        }
 
     @staticmethod
     def calc_multiply(first, second=None):
         s = first * second
-        print(s)
+        return s
 
     @staticmethod
     def calc_divide(first, second=None):
         try:
-            s = first / second
-            print(s)
+            if second == 0:
+                print("Деление на ноль!")
+                return 0  # Возвращаем 0 вместо None
+            return first / second
         except ZeroDivisionError:
-            print("Деление на ноль!")
+            print("Ошибка деления на ноль!")
+            return 0
 
     @staticmethod
     def calc_subtract(first, second=None):
         s = first - second
-        print(s)
+        return s
 
     @staticmethod
     def calc_add(first, second=None):
         if second is None:
-            print(sum(first))
+            return sum(first)
         else:
-            print(first + second)
+            return first + second
 
-    @staticmethod
-    def memo_plus(number=None):
-        pass
+    def set_info(self):
+        self.num_1 = input('Введи цифру или математическое выражение без пробелов: ')
+        self.operation = input('Выберите знак математической операции: +, -, *, /  ')
+        self.num_2 = input('Введи цифру: ')
 
-    @staticmethod
-    def memo_minus():
-        pass
+    def check_input(self):
+        match = re.fullmatch(self.pattern, self.num_1)
+        if match:
+            first_num, _, operation, second_num, _ = match.groups()
+            first_num = float(first_num)
+            second_num = float(second_num)
+            result = self.operations[operation](first_num, second_num)
+            print(result)
+            self.flag_expression = True
+            self.last_result = result
+            BasicCalc.last_result = result
+            return result
 
-    @property
-    def top_memory(self):
-        if self.memory:
-            return self.memory[-1]
-        return 'Список пуст!'
-
-    def run(self):
-        pass
-
-
-operations = {
-    '+': BasicCalc.calc_add,
-    '-': BasicCalc.calc_subtract,
-    '*': BasicCalc.calc_multiply,
-    '/': BasicCalc.calc_divide
-}
-
-pattern = BasicCalc.pattern
-flag_expression = False
-flag_letters = False
-flag_dot = False
-flag_sp = False
-operation = None
-num_2 = None
-
-while True:
-    num_1 = input('Введи цифру или математическое выражение без пробелов: ')
-    match = re.fullmatch(pattern, num_1)
-
-    if match:
-        first_num, _, operation, second_num, _ = match.groups()
-        first_num = float(first_num) if '.' in first_num else int(first_num)
-        second_num = float(second_num) if '.' in second_num else int(second_num)
-        operations[operation](first_num, second_num)
-        flag_expression = True
-        break
-    else:
-        for n in num_1:
-            n.replace('.', '', 1)
-            if n.isalpha():
-                flag_letters = True
-                break
-            elif n == '.':
-                flag_dot = True
-                break
-        if flag_letters is True or flag_dot is True:
-            print('Недопустимая операция!')
-            flag_letters = False
-            flag_dot = False
-            continue
-        else:
-            if len(num_1) > 1 and ' ' in num_1:
-                num_1 = [int(n) for n in num_1.split()]
-                flag_sp = True
-                break
-            elif '.' in num_1:
-                num_1 = float(num_1)
-                break
-            elif '.' not in num_1:
-                num_1 = int(num_1)
-                break
-
-if flag_expression is False:
-    while True:
-        operation = input('Выберите знак математической операции: +, -, *, /  ')
-        if operation not in operations:
-            print('Недопустимая операция!')
-            continue
-        else:
-            break
-
-    if flag_sp is False:
         while True:
-            num_2 = input('Введи цифру: ')
-            for n in num_2:
+            for n in self.num_1:
                 n.replace('.', '', 1)
-                if n.isalpha():
-                    flag_letters = True
+                if n.isalpha() or n == '.':
+                    print(f'Некорректное значение "{self.num_1}"')
+                    self.num_1 = input('Повтори ввод первого числа: ')
+                    self.num_1_invalid = True
                     break
-                elif n == '.':
-                    flag_dot = True
-                    break
-            if flag_letters is True or flag_dot is True:
-                print('Hедопустимая операция!')
-                flag_letters = False
-                flag_dot = False
+            if self.num_1_invalid:
+                self.num_1_invalid = False
                 continue
-            else:
-                if '.' in num_2:
-                    num_2 = float(num_2)
-                    break
-                elif '.' not in num_2:
-                    num_2 = int(num_2)
-                    break
 
-if flag_expression is False:
-    if flag_sp:
-        operations[operation](num_1)
-    else:
-        operations[operation](num_1, num_2)
+            if len(self.num_1) > 1 and ' ' in self.num_1:
+                self.num_1 = [int(n) for n in self.num_1.split()]
+                self.flag_sp = True
+                break
+            else:
+                self.num_1 = float(self.num_1)
+                break
+
+        while True:
+            if self.flag_sp is False:
+                for n in self.num_2:
+                    n.replace('.', '', 1)
+                    if n.isalpha() or n == '.':
+                        print(f'Некорректное значение "{self.num_2}"')
+                        self.num_2 = input('Повтори ввод второго числа: ')
+                        self.num_2_invalid = True
+                        break
+                if self.num_2_invalid:
+                    self.num_2_invalid = False
+                    continue
+
+                self.num_2 = float(self.num_2)
+                break
+
+    def calculate_result(self):
+        if self.flag_expression is False:
+            if self.flag_sp:
+                result = self.operations[self.operation](self.num_1)
+            else:
+                result = self.operations[self.operation](self.num_1, self.num_2)
+
+            print(result)
+            BasicCalc.last_result = result
+            return result
+
+
+if __name__ == '__main__':
+    calc = BasicCalc()
+    calc.set_info()
+    calc.check_input()
+    calc.calculate_result()
