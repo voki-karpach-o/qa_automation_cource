e049cfb4ba55d859c1fbb08c6bec1215778d31f1
Карпач Олег <oleg.karpach@vokigames.com>
2025-06-11 17:31:00 +0400

    1) Fixed the code 2) Fixed logic of the exceptions

--- basic_calc.py
+++ basic_calc.py
@@ -57,55 +57,54 @@ class BasicCalc:
             first_num, _, operation, second_num, _ = match.groups()
             first_num = float(first_num)
             second_num = float(second_num)
-            result = self.operations[operation](first_num, second_num)
-            print(result)
+            calculated_result = self.operations[operation](first_num, second_num)
+            print(calculated_result)
             self.flag_expression = True
-            self.last_result = result
-            return result
+            self.last_result = calculated_result
+            return calculated_result
 
-        try:
-            for n in self.num_1:
+        for n in self.num_1:
+            n.replace('.', '', 1)
+            if n.isalpha() or n == '.':
+                raise ValueError("Введено неправильное значения для первого числа!")
+
+            if len(self.num_1) > 1 and ' ' in self.num_1:
+                self.num_1 = [int(n) for n in self.num_1]
+                self.flag_sp = True
+
+            else:
+                self.num_1 = float(self.num_1)
+
+        if self.flag_sp is False:
+            for n in self.num_2:
                 n.replace('.', '', 1)
                 if n.isalpha() or n == '.':
-                    self.num_1_invalid = True
-                    raise ValueError
-
-                if len(self.num_1) > 1 and ' ' in self.num_1:
-                    self.num_1 = [int(n) for n in self.num_1]
-                    self.flag_sp = True
+                    self.num_2_invalid = True
+                    raise ValueError("Введено неправильное значения для первого числа!")
 
                 else:
-                    self.num_1 = float(self.num_1)
+                    self.num_2 = float(self.num_2)
 
-        except ValueError:
-            print(f'Некорректное значение "{self.num_1}"')
-
-        try:
-            if self.flag_sp is False:
-                for n in self.num_2:
-                    n.replace('.', '', 1)
-                    if n.isalpha() or n == '.':
-                        self.num_2_invalid = True
-                        raise ValueError
-
-                    else:
-                        self.num_2 = float(self.num_2)
-
-        except ValueError:
-            print(f'Некорректное значение "{self.num_2}"')
-
-        if self.flag_expression is False and not self.num_1_invalid and not self.num_2_invalid:
+        if self.flag_expression is False:
             if self.flag_sp:
-                result = self.operations[self.operation](self.num_1)
+                calculated_result = self.operations[self.operation](self.num_1)
             else:
-                result = self.operations[self.operation](self.num_1, self.num_2)
+                calculated_result = self.operations[self.operation](self.num_1, self.num_2)
 
-            print(result)
-            self.last_result = result
-            return result
+            print(calculated_result)
+            self.last_result = calculated_result
+            return calculated_result
 
 
 if __name__ == '__main__':
     calc = BasicCalc()
     calc.input_info()
-    calc.check_and_calculate_result()
+
+    try:
+        result = calc.check_and_calculate_result()
+        if result is not None:
+            print(f"Итоговый результат: {result}")
+    except ValueError as v:
+        print(f"Произошла ошибка ввода: {v}")
+    except Exception as e:
+        print(f"Произошла непредвиденная ошибка: {e}")
